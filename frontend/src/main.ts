6920274b37b5f5265890dbecbe4d6466dd62c55f
Ido Mandelman <idomand@gmail.com>
2025-09-14 14:19:46 +0200

    update vuetify import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style/global.css";
 import App from "./App.vue";
 import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
+import "vuetify/styles/main.css";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
