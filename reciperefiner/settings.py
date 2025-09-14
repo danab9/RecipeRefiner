76be756a920bd672f1fb9f4b9eccb92041432d23
danabarilan <danabr93@gmail.com>
2025-09-14 21:36:09 +0200

    fixing CORS

--- reciperefiner/settings.py
+++ reciperefiner/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "input",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # React/Vue/Angular dev server
+#     "http://127.0.0.1:3000",
+# ]
+# For now allow all: TODO change before production
+CORS_ALLOW_ALL_ORIGINS = True
