b87e11aaae5e500e22b054ecd10534a536d1c463
danabarilan <danabr93@gmail.com>
2025-09-15 14:39:39 +0000

    refactor get_url view to handle JSON input and return JsonResponse

--- input/views.py
+++ input/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-from .forms import URLFrom
+# from .forms import URLFrom
 
 from .services.recipe_processor import scrape_recipe
 
 def get_url(request):
     if request.method == "POST":
-        form = URLFrom(request.POST)
-        if form.is_valid():
-            data = scrape_recipe(form.cleaned_data["url"])
-            return render(request, "recipe_result.html", {"recipe": data})
-    else:
-        form = URLFrom()
-    
-    # default behaviour, if no "POST" response overrides it
-    return render(request, "url.html", {"form": form})
+        try:
+            body = json.load(request.body)
+            url = body.get("url")
+            if not url:
+                return JsonResponse({"error": "Miising 'url' field"}, status=400)
+
+            data = scrape_recipe(url)
+            return JsonResponse({{"recipe": data}})
+        
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+    else:  # GET
+        return JsonResponse({}) # TODO
+
