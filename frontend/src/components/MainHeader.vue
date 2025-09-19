d85982714b04fc95187fb0e3147379c4f62a3336
Ido Mandelman <idomand@gmail.com>
2025-09-19 21:51:53 +0200

    feat: update app branding and replace Vite logo with food icon; add RecipeCard component

--- frontend/src/components/MainHeader.vue
+++ frontend/src/components/MainHeader.vue
@@ -2,89 +2,70 @@
   <v-container class="mt-10">
     <v-row>
       <v-col>
-        <h1 class="text-center">Dana's app</h1>
+        <h1 class="text-center">Dana's Recipe Refiner</h1>
       </v-col>
     </v-row>
-
     <v-row>
       <v-col>
-        <v-form ref="form" @submit.prevent="submitForm" v-model="valid">
-          <v-text-field
-            clearable
-            label="Please enter URL"
-            prepend-icon="mdi-silverware-fork-knife"
-            variant="outlined"
-            :rules="rules"
-            v-model="URL"
-          />
+        <v-row class="d-flex align-center">
+          <v-col cols="10">
+            <v-text-field
+              clearable
+              label="Please enter URL"
+              prepend-icon="mdi-silverware-fork-knife"
+              variant="outlined"
+              v-model="URL"
+              hide-details
+            />
+          </v-col>
           <v-btn
-            class="mt-2"
+            class="align-center"
             type="submit"
-            block
-            :disabled="!valid"
+            :disabled="!isValidURL"
             @click="submitUrl"
-          >
-            Submit
-          </v-btn>
-        </v-form>
+            text="submit"
+            height="56"
+          />
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
-  <pre>{{ recipe }}</pre>
-  <DogPicture />
+  <RecipeCard :recipe="recipe" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import DogPicture from "@/components/DogPicture.vue";
 import axios from "axios";
+import RecipeCard from "./RecipeCard.vue";
 
 type Recipe = {
-  ingredients: string;
+  ingredients: string[];
   instructions: string;
   title: string;
 };
 
 export default defineComponent({
   name: "MainHeader",
-  components: { DogPicture },
+  components: { RecipeCard },
   data() {
     return {
       recipe: {} as Recipe,
       URL: "",
-      valid: false,
-      rules: [
-        (value: string) => {
-          if (!value) return "You must enter a URL.";
-          return this.isValidURL(value) || "Please enter a valid URL.";
-        },
-      ],
+      isLoading: false,
     };
   },
-  computed: {},
-  mounted() {},
-  methods: {
-    isValidURL(string: string): boolean {
+  computed: {
+    isValidURL(): boolean {
       try {
-        const url = new URL(string);
+        const url = new URL(this.URL);
         return url.protocol === "http:" || url.protocol === "https:";
       } catch (_) {
         return false;
       }
     },
-
-    async submitForm() {
-      // Validate the form before submission
-      const isValid = await (this.$refs.form as any).validate();
-
-      if (isValid.valid) {
-        console.log("Valid URL submitted:", this.URL);
-        // Add your submission logic here
-        // Example: this.sendToAPI(this.URL);
-      } else {
-        console.log("Form validation failed");
-      }
-    },
+  },
+  mounted() {},
+  methods: {
     async submitUrl() {
       const response = await axios.post("http://localhost:8000/", {
         url: this.URL,
@@ -92,6 +73,7 @@ export default defineComponent({
 
       if (response.status === 200) {
         this.recipe = response.data.recipe;
+        this.URL = "";
       }
 
       console.log("response :>> ", response);
---
+++ frontend/src/components/RecipeCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="recipe?.title">
+    <v-card :title="recipe.title" variant="outlined">
+      <v-row>
+        <v-col>
+          <v-list density="compact" :items="recipe.ingredients"></v-list>
+        </v-col>
+        <v-col>
+          <p class="text-h5">{{ recipe.instructions }}</p>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+type Recipe = {
+  ingredients: string[];
+  instructions: string;
+  title: string;
+};
+export default defineComponent({
+  name: "RecipeCard",
+  components: {},
+  props: {
+    recipe: {
+      type: Object as PropType<Recipe>,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style scoped></style>
