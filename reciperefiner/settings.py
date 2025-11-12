ae1ca3dfb2dd26a896c1d2b8c8efe94d477a401c
danabarilan <danabr93@gmail.com>
2025-11-12 10:38:10 +0000

    update ALLOWED_HOSTS to use environment variable

--- reciperefiner/settings.py
+++ reciperefiner/settings.py
@@ -22,8 +22,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = bool(os.environ.get("DEBUG", default=0))  # safer for production
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # Application definition
 
